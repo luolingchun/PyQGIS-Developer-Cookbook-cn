e3a36c3adc61b07c68c59a953466247bde19ace1
luolingchun <luolingchun@outlook.com>
2023-04-27 10:55:42 +0800

    update mkdocs.yml

--- docs/js/db.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?232e9ef21b9f69b563661b2c1adc416e";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
